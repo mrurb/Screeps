ca3bfeb5728a7f466f60ad9bfb9b142563172731
Anders Urban <minikaliffen@gmail.com>
2016-10-08 23:37:25 +0200

    stuff 2

---
+++ Working_v5/role.massupgrader.js
@@ -0,0 +1,37 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('harvesting');
+        }
+        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+            creep.say('upgrading');
+        }
+
+        if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+               // creep.moveTo(creep.room.controller);
+                creep.moveTo(Game.flags.UpgradeFlag)
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER) && _.sum(structure.store) < structure.storeCapacity;
+                    }
+            });
+            //console.log(targets);
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;
