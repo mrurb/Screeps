f60e1f44d3971a9c80d8e8b992987200a25e1b97
Webba <webba93@gmail.com>
2016-10-08 22:59:03 +0100

    Updated Spawner

--- Webba/behavior.spawn.js
+++ Webba/behavior.spawn.js
@@ -7,12 +7,16 @@
  * mod.thing == 'a thing'; // true
  */
 var spawnBehavior = {
-	getBodyParts: function(spawn, partRatios, energyPercentage){
-		var maxEnergy = spawn.room.energyCapacityAvailable;
-		var costPerRatio = _.sum(_.map(partRatios, function(no, part){
-			return BODYPART_COST[part] * no;
-		}));
-		var multiplier = Math.floor(maxEnergy * energyPercentage / costPerRatio);
+	getBodyParts: function(spawn, useRatio, partRatios, energyPercentage){
+		var multiplier = 1;
+		if (useRatio)
+		{
+			var maxEnergy = spawn.room.energyCapacityAvailable;
+			var costPerRatio = _.sum(_.map(partRatios, function(no, part){
+				return BODYPART_COST[part] * no;
+			}));
+			multiplier = Math.floor(maxEnergy * energyPercentage / costPerRatio);
+		}
 		var body = [];
 		for (var part in partRatios){
 			body = body.concat(_.times(partRatios[part] * multiplier, _.constant(part)));
@@ -21,45 +25,44 @@ var spawnBehavior = {
 	},
 
 	spawnCreep: function(spawn, creepType){
-		var memWrite = {role: creepType};
-		if (creepType == "miner"){
-			memwrite.source = 0;
-		}
-		spawn.createCreep(this.getBodyParts(spawn, spawn.memory.creepParts[creepType], spawn.memory.energyPercentage[creepType]), undefined, memWrite);
+		var memWrite = {role: creepType, spawnedBy: spawn.id};
+		return spawn.createCreep(this.getBodyParts(spawn, spawn.memory.template[creepType].useRatio, spawn.memory.template[creepType].parts, spawn.memory.template[creepType].energyRatio), undefined, memWrite);
 	},
 
 	pollSpawn: function(spawn){
-		for (var role in spawn.memory.creepMax){
-			if(spawn.memory.creepMax[role] > this.getCreepCount(spawn, role)){
-				this.spawnCreep(spawn, role);
-				break;
+		for (var role in spawn.memory.template){
+			if(spawn.memory.template[role].maxCount > this.getCreepCount(spawn, role)){
+				return this.spawnCreep(spawn, role);
 			}
 		}
+		return undefined;
 	},
 
 	getCreepCount: function(spawn, creepType){
-		return _.filter(spawn.room.find(FIND_MY_CREEPS), function(creep){ return creep.memory.role == creepType }).length;
+		return _.filter(Game.creeps, function(creep){ return (creep.memory.role == creepType && creep.memory.spawnedBy == spawn.id) }).length;
 	},
 
 	initSpawn: function(spawn){
-		spawn.memory.creepParts = {};
-		spawn.memory.creepParts.harvester = { work: 1, move: 1, carry: 1};
-		spawn.memory.creepParts.upgrader = { work: 1, move: 1, carry: 1};
-		spawn.memory.creepParts.builder = { work: 1, move: 1, carry: 1};
-		spawn.memory.creepParts.repairer = { work: 1, move: 1, carry: 1};
-		spawn.memory.creepParts.miner = { work: 2, move: 1, carry: 1};
-		spawn.memory.creepMax = {};
-		spawn.memory.creepMax.harvester = 1;
-		spawn.memory.creepMax.upgrader = 2;
-		spawn.memory.creepMax.builder = 2;
-		spawn.memory.creepMax.repairer = 1;
-		spawn.memory.creepMax.miner = 2;
-		spawn.memory.energyPercentage = {};
-		spawn.memory.energyPercentage.harvester = 1;
-		spawn.memory.energyPercentage.upgrader = 1;
-		spawn.memory.energyPercentage.builder = 1;
-		spawn.memory.energyPercentage.repairer = 1;
-		spawn.memory.energyPercentage.miner = 1;
+		spawn.memory.template.harvester = {}
+		spawn.memory.template.harvester.useRatio = true;
+		spawn.memory.template.harvester.parts = { work: 1, move: 1, carry: 1};
+		spawn.memory.template.harvester.maxCount = 2;
+		spawn.memory.template.harvester.energyRatio = 0.5;
+		spawn.memory.template.upgrader = {}
+		spawn.memory.template.upgrader.useRatio = true;
+		spawn.memory.template.upgrader.parts = { work: 1, move: 1, carry: 1};
+		spawn.memory.template.upgrader.maxCount = 2;
+		spawn.memory.template.upgrader.energyRatio = 0.5;
+		spawn.memory.template.builder = {}
+		spawn.memory.template.builder.useRatio = true;
+		spawn.memory.template.builder.parts = { work: 1, move: 1, carry: 1};
+		spawn.memory.template.builder.maxCount = 1;
+		spawn.memory.template.builder.energyRatio = 0.5;
+		spawn.memory.template.miner = {}
+		spawn.memory.template.miner.useRatio = true;
+		spawn.memory.template.miner.parts = { work: 2, move: 1, carry: 1};
+		spawn.memory.template.miner.maxCount = 0;
+		spawn.memory.template.miner.energyRatio = 0.5;
 	}
 }
 
